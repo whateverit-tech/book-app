6b0ee4a4fedbf78659f0548f11f126e207b51ec2
Rahmat Kurniawan <rahmat.kurniawan@mbiz.co.id>
2022-05-16 13:39:56 +0700

    change index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from "react";
+import ReactDOM from "react-dom/client";
 import './index.css';
 import App from './App';
 import { store } from './redux/store'
 import { Provider } from 'react-redux'
 
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
 
-
-ReactDOM.render(
+root.render(
   <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
